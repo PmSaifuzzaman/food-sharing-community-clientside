f560dc8226aac1744670311d943a8555fed2c8a9
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-04 20:06:50 +0600

    Clientside favicon,React Router,Tailwind Css, Daisy UI added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='text-5xl text-red-500'>pmsaifuzzaman</h1>
+      
+    </>
+  )
+}
+
+export default App
